03e98557902a2deb7099d45af7b6930861074fd4
PJ Murray <pj.murray.nz@gmail.com>
2018-06-04 12:05:45 +1200

    add tenets/php/sql_injection

---
+++ tenets/codelingo/php/sql-concats/example.php
@@ -0,0 +1,11 @@
+<?php
+function ExecuteSQL ($query) {
+	return "SQL result\n";
+}
+function Main() {
+	$userNamesSQL = "SELECT name FROM Users";
+	$userNames = ExecuteSQL($userNamesSQL);
+	print $userNames;
+}
+Main();
+php>
