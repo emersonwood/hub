de1886a6ae6f67255b5f74a172cf29f5ad5b6e05
BlakeMScurr <blakemscurr@gmail.com>
2018-06-19 11:07:14 +1200

    Fix wide ranging regex in tenets/php/sql_injection
    Capture more cases

--- tenets/codelingo/php/sql-concats/example.php
+++ tenets/codelingo/php/sql-concats/example.php
@@ -3,9 +3,15 @@ function ExecuteSQL ($query) {
 	return "SQL result\n";
 }
 function Main() {
-	$userNamesSQL = "SELECT name FROM Users";
+	$userTable = "USERS";
+	$userNamesSQL = "SELECT name FROM " . $userTable; // ISSUE
 	$userNames = ExecuteSQL($userNamesSQL);
 	print $userNames;
+
+	$sql = "SELECT cost FROM ";
+	$sql .= "PRODUCTS"; // ISSUE
+	$products = ExecuteSQL($sql);
+	print $$products;
 }
 Main();
 php>
